10363237d7d7b0d38a949c495e95a289f243731b
h-dianne <diannehidalgo117@gmail.com>
2025-09-12 11:58:23 +0900

    refactor: Organize main function by setting window dimensions

--- src/main.py
+++ src/main.py
@@ -10,8 +10,16 @@ def main(page: ft.Page) -> None:
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.scroll = ft.ScrollMode.ADAPTIVE
 
-    # create app control and add it to the page
-    page.add(TodoApp())
+    # Set window dimensions
+    page.window.width = 1600
+    page.window.height = 900
+    page.window.min_width = 1280
+    page.window.min_height = 720
+
+    # Main App Component
+    todo_app = TodoApp()
+    todo_app.width = 900
+    page.add(todo_app)
 
 
 if __name__ == "__main__":
